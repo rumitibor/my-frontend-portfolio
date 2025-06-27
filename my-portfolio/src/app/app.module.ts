a9c214cd1b532b07fdfcdf9ca4096876b1ad8d4a
rumitibor <rumitibor@gmail.comgit config --global user.name Your>
2025-06-27 18:27:57 +0200

    projects

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { CvComponent } from './components/cv/cv/cv.component';
 import { AboutComponent } from './components/about/about/about.component';
 import { ContactComponent } from './components/contact/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects/projects.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, FooterComponent, CvComponent, AboutComponent, ContactComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, FooterComponent, CvComponent, AboutComponent, ContactComponent, ProjectsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
