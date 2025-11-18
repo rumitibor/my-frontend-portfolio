e2f77b2e0610d7496e4e8a3315f504d86c43b2ed
rumitibor <rumitibor@gmail.comgit config --global user.name Your>
2025-11-18 17:05:39 +0100

    design changes

--- my-portfolio/src/app/components/projects/projects/projects.component.ts
+++ my-portfolio/src/app/components/projects/projects/projects.component.ts
@@ -40,20 +40,64 @@ export class ProjectsComponent {
       tech: [
         { name: 'Angular', link: '/tags/angular' },
         { name: 'Firebase', link: 'https://firebase.google.com/' },
+        { name: 'TypeScript' },
         { name: 'SCSS' },
+        { name: 'Bootstrap' },
+        { name: 'Rest API' },
+        { name: 'Authorization' },
       ],
     },
     {
       title: 'My Portfolio',
       img: './img/portfolio-project.png',
-      demo: 'https://my-portfolio.web.app/',
+      demo: 'https://rumitibor.github.io/my-frontend-portfolio/#/',
       description: 'A personal portfolio built with Angular 16 and SCSS. ',
 
       showDescription: false,
       tech: [
         { name: 'Angular', link: '/tags/angular' },
         { name: 'Firebase', link: 'https://firebase.google.com/' },
+        { name: 'Bootstrap' },
+        { name: 'API' },
         { name: 'SCSS' },
+        { name: 'TypeScript' },
+      ],
+    },
+    {
+      title: 'Bamboo Webshop',
+      img: './img/bg2.jpg',
+      demo: 'https://github.com/rumitibor/bamboo-webshop',
+      description: 'Bamboo webshop built with Angular 16 and SCSS.  ',
+
+      showDescription: false,
+      tech: [
+        { name: 'Angular', link: '/tags/angular' },
+        { name: 'Firebase', link: 'https://firebase.google.com/' },
+        { name: 'Authorization' },
+        { name: 'Bootstrap' },
+        { name: 'TypeScript' },
+        { name: 'SCSS' },
+      ],
+    },
+    {
+      title: 'Progmasters Bootcamp Projects',
+      img: './img/bg3.jpg',
+      demo: 'https://github.com/PROGmasters-Bootcamp-2024-Okt-Frontend',
+      description:
+        'My Github projects at Progmasters (Frontend Developer Academy).  ',
+
+      showDescription: false,
+      tech: [
+        { name: 'Angular', link: '/tags/angular' },
+        { name: 'Firebase', link: 'https://firebase.google.com/' },
+        { name: 'JavaScript' },
+        { name: 'Bootstrap' },
+        { name: 'TypeScript' },
+        { name: 'CSS' },
+        { name: 'SCSS' },
+        { name: 'HTML5' },
+        { name: 'VUE' },
+        { name: 'React' },
       ],
     },
   ];
